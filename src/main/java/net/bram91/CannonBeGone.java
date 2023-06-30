00d601b51eaa6e430e3bf2b43064ab8368b80042
bram <bramvgemert@gmail.com>
2023-06-30 17:34:04 +0200

    use ongameobjectspawned

--- src/main/java/net/bram91/CannonBeGone.java
+++ src/main/java/net/bram91/CannonBeGone.java
@@ -4,6 +4,7 @@ import com.google.inject.Provides;
 import javax.inject.Inject;
 import lombok.extern.slf4j.Slf4j;
 import net.runelite.api.*;
+import net.runelite.api.events.GameObjectSpawned;
 import net.runelite.api.events.GameTick;
 import net.runelite.client.callback.ClientThread;
 import net.runelite.client.eventbus.Subscribe;
@@ -25,10 +26,7 @@ public class CannonBeGone extends Plugin
 	@Override
 	protected void startUp() throws Exception
 	{
-		if (client.getGameState() == GameState.LOGGED_IN)
-		{
-			clientThread.invoke(this::hide);
-		}
+
 	}
 
 	@Override
@@ -44,46 +42,20 @@ public class CannonBeGone extends Plugin
 	}
 
 	@Subscribe
-	public void onGameTick(GameTick tick)
+	public void onGameObjectSpawned(GameObjectSpawned event)
 	{
-		hide();
-	}
-
-	private void hide()
-	{
-		Scene scene = client.getScene();
-		Tile[][] tiles = scene.getTiles()[0];
-		for (int x = 0; x < Constants.SCENE_SIZE; ++x)
-		{
-			for (int y = 0; y < Constants.SCENE_SIZE; ++y)
-			{
-				Tile tile = tiles[x][y];
-				if (tile == null)
-				{
-					continue;
-				}
-
-				for (GameObject gameObject : tile.getGameObjects())
-				{
-					if (gameObject != null)
-					{
-						switch(gameObject.getId()) {
-							case ObjectID.BROKEN_MULTICANNON:
-							case ObjectID.BROKEN_MULTICANNON_14916:
-							case ObjectID.BROKEN_MULTICANNON_43028:
-							case ObjectID.DWARF_MULTICANNON:
-							case ObjectID.DWARF_MULTICANNON_11868:
-							case ObjectID.DWARF_MULTICANNON_35885:
-							case ObjectID.DWARF_MULTICANNON_35886:
-							case ObjectID.DWARF_MULTICANNON_43027:
-							case ObjectID.DWARF_MULTICANNON_5975:
-							case ObjectID.DWARF_MULTICANNON_5976:
-								scene.removeGameObject(gameObject);
-						}
-						break;
-					}
-				}
-			}
+		switch(event.getGameObject().getId()) {
+			case ObjectID.BROKEN_MULTICANNON:
+			case ObjectID.BROKEN_MULTICANNON_14916:
+			case ObjectID.BROKEN_MULTICANNON_43028:
+			case ObjectID.DWARF_MULTICANNON:
+			case ObjectID.DWARF_MULTICANNON_11868:
+			case ObjectID.DWARF_MULTICANNON_35885:
+			case ObjectID.DWARF_MULTICANNON_35886:
+			case ObjectID.DWARF_MULTICANNON_43027:
+			case ObjectID.DWARF_MULTICANNON_5975:
+			case ObjectID.DWARF_MULTICANNON_5976:
+				client.getScene().removeGameObject(event.getGameObject());
 		}
 	}
 }
